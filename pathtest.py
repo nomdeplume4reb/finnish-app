bc65e6c32db0d4df208ba2ab6c5da245ba104746
nomdeplume4reb <48643044+nomdeplume4reb@users.noreply.github.com>
2021-08-22 11:44:20 +0300

    testing voikko stuff

--- pathtest.py
+++ pathtest.py
@@ -4,24 +4,28 @@ import os
 
 dirname = os.path.dirname(os.path.abspath(__file__))
 voikkoPath = 'r'+'"'+os.path.join(dirname, 'Voikko')+'"'
-voikkoDictPath = 'r'+'"'+os.path.join(dirname, 'Voikko\dict')+'"'
-# print(voikkoPath)
-# print('r"C:\\Users\\rache\\Downloads\\data_science\\finnish-app\\Voikko"')
-# print(voikkoDictPath)
-# print('r"C:\\Users\\rache\\Downloads\\data_science\\finnish-app\\Voikko\\dict"')
+voikkoDictPath = 'r'+'"'+os.path.join(dirname, 'Voikko\\dict')+'"'
 
 # Voikko.setLibrarySearchPath(r"C:\Users\rache\Downloads\data_science\finnish-app\Voikko")
-# v = libvoikko.Voikko("fi", r"C:\Users\rache\Downloads\data_science\finnish-app\Voikko\dict") # ERROR: 'Voikko' object has no attribute '_Voikko__handle'
-
-#v = libvoikko.Voikko(u"fi") # 2 ERRORs: 'Voikko' object has no attribute '_Voikko__handle' AND code=H10 desc="App crashed" method=GET path="/favicon.ico
+# v = libvoikko.Voikko("fi", r"C:\Users\rache\Downloads\data_science\finnish-app\Voikko\dict")
 
 Voikko.setLibrarySearchPath(voikkoPath)
-v = libvoikko.Voikko("fi", voikkoDictPath) # ERROR: 'Voikko' object has no attribute '_Voikko__handle'
+v = libvoikko.Voikko("fi", voikkoDictPath)
 
-v = libvoikko.Voikko("fi") # ERROR: 'Voikko' object has no attribute '_Voikko__handle'
 
-v = Voikko("fi")  # 2 ERRORs: 'Voikko' object has no attribute '_Voikko__handle' AND code=H10 desc="App crashed" method=GET path="/favicon.ico
+#
+# # v = libvoikko.Voikko("fi") # ERROR: 'Voikko' object has no attribute '_Voikko__handle'
+# #
+# # v = Voikko("fi")  # 2 ERRORs: 'Voikko' object has no attribute '_Voikko__handle' AND code=H10 desc="App crashed" method=GET path="/favicon.ico
+#
+# lemmatized = v.analyze('kissa')[0]
+#
+# print(lemmatized)
 
-lemmatized = v.analyze('kissa')[0]
 
-print(lemmatized)
+# Voikko.setLibrarySearchPath(voikkoPath)
+# v = libvoikko.Voikko("fi", r"C:\Users\rache\Downloads\data_science\finnish-app\Voikko\dict") # ERROR: 'Voikko' object has no attribute '_Voikko__handle'
+#
+#
+# #v = libvoikko.Voikko(u"fi") # 2 ERRORs: 'Voikko' object has no attribute '_Voikko__handle' AND code=H10 desc="App crashed" method=GET path="/favicon.ico
+#
